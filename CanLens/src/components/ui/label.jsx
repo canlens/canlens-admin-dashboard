c81c2a64f8c4fad1f95587d616177aa5c365bca8
Mustafa Khamis <safsafrwanda2006@gmail.com>
2026-06-06 07:52:59 +0200

    update the new file

---
+++ CanLens/src/components/ui/label.jsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cn } from "./utils";
+
+function Label({ className, ...props }) {
+  return (
+    <LabelPrimitive.Root
+      data-slot="label"
+      className={cn("label", className)}
+      {...props}
+    />
+  );
+}
+
+export { Label };
